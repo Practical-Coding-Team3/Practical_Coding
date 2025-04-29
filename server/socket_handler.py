040dd4425eadee508154fc353b00a263676c9a3c
wqdsdsf <chlxodud04@naver.com>
2025-04-29 23:35:03 +0900

    ...

--- server/socket_handler.py
+++ server/socket_handler.py
@@ -1,31 +1,31 @@
-import asyncio
-import io
-from aiohttp import web
-import os
-from pydub import AudioSegment
-from whisper_transcriber import onpenai_speech
+# import asyncio
+# import io
+# from aiohttp import web
+# import os
+# from pydub import AudioSegment
+# from whisper_transcriber import onpenai_speech
 
-async def handle_media_stream(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-    print("✅ WebSocket Client Connected")
+# async def handle_media_stream(request):
+#     ws = web.WebSocketResponse()
+#     await ws.prepare(request)
+#     print("✅ WebSocket Client Connected")
 
-    audio_buffer = io.BytesIO()
+#     audio_buffer = io.BytesIO()
 
-    async for msg in ws:
-        if msg.type == web.WSMsgType.BINARY:
-            audio_data = msg.data
-            print(f"🎵 Received {len(audio_data)} bytes of audio data")
-            audio_buffer.write(audio_data)
+#     async for msg in ws:
+#         if msg.type == web.WSMsgType.BINARY:
+#             audio_data = msg.data
+#             print(f"🎵 Received {len(audio_data)} bytes of audio data")
+#             audio_buffer.write(audio_data)
 
-    # WebSocket 종료 후 실행되도록 수정
-    total_bytes = audio_buffer.getbuffer().nbytes
+#     # WebSocket 종료 후 실행되도록 수정
+#     total_bytes = audio_buffer.getbuffer().nbytes
 
-    if total_bytes > 0:
-        audio_buffer.seek(0)
-        audio_segment = AudioSegment.from_file(audio_buffer, format="webm")
-        audio_segment.export("output.wav", format="wav")
-        onpenai_speech("./output.wav")
+#     if total_bytes > 0:
+#         audio_buffer.seek(0)
+#         audio_segment = AudioSegment.from_file(audio_buffer, format="webm")
+#         audio_segment.export("output.wav", format="wav")
+#         onpenai_speech("./output.wav")
 
-    print("❌ WebSocket Client Disconnected")
-    return ws
+#     print("❌ WebSocket Client Disconnected")
+#     return ws
